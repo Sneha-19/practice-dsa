410928a431ca64efee36f3d2fcdbe804276d1424
Sneha-19 <19snehasingh97@gmail.com>
2025-06-18 21:38:50 +0530

    Linear search

---
+++ Searching_Sorting/linearSearch.js
@@ -0,0 +1,9 @@
+function linearSearch(arr, target){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] === target) return i;
+       
+    }
+    return -1;
+}
+
+console.log(linearSearch([1,4,6,7,0,9], 7))
